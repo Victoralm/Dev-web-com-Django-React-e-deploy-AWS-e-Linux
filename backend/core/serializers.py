3d39b0f8f2429c11e8128cb69e730269d2f4e42d
Victoralm <Victoralm@users.noreply.github.com>
2025-01-06 19:42:56 -0300

    Writable Nested Serializers - setando "item_set" com atributo "read_only=True"

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -4,14 +4,14 @@ from .models import List, Item
 class ItemSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Item
-        fields = ['url', 'name', 'list', 'done']
+        fields = ['url', 'name', 'done']
 
 
 class ListSerializer(serializers.HyperlinkedModelSerializer):
     # Notar que foi necessário alterar a ordem em que as classes aparecem no arquivo.
     # A classe ItemSerializer é referenciada na classe ListSerializer, então a classe ItemSerializer
     # deve ser declarada antes da classe ListSerializer. Caso contrário, o código não funcionará. 🤦‍♂️
-    item_set = ItemSerializer(many=True)
+    item_set = ItemSerializer(many=True, read_only=True)
     class Meta:
         model = List
         # fields = '__all__'
